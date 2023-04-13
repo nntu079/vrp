df1b9734defb52de41567c3ffe9b902cc28d6661
Tu <nntu079>
2023-04-14 05:55:56 +0700

    a

--- util.js
+++ util.js
@@ -1,10 +1,16 @@
 const { Pool } = require('pg')
 
-const connectionString = "postgres://nntu079@ep-twilight-violet-887854.ap-southeast-1.aws.neon.tech/neondb"
 
 let conn;
 if (!conn) {
-  conn = new Pool(connectionString);
+  conn = new Pool({
+    user: "nntu079",
+    password: "TKLoYvBh39xH",
+    host: "ep-twilight-violet-887854.ap-southeast-1.aws.neon.tech",
+    port: 5432,
+    database: "neondb",
+    ssl: true,
+  });
 }
 
 module.exports = conn;
